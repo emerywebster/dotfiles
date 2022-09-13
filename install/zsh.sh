687c796484ddab33e1f96fb48fbe1c4fe83eb9d9
Webb Henderson <webb.henderson@gmail.com>
2022-09-13 11:09:52 -0600

    Auto-install zsh theme

--- install/zsh.sh
+++ install/zsh.sh
@@ -2,3 +2,7 @@ brew install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 chsh -s /bin/zsh
+
+# zsh theme
+# https://github.com/romkatv/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
